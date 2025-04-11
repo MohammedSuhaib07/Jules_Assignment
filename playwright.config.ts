06e2210cbcfbbc8ba5f6a0bd7beeea2466e00a9f
MohammedSuhaib07 <shaik.md.suhaib@gmail.com>
2025-04-11 17:57:26 +0530

    Added GitHub Actions workflow for Playwright tests and report publishing

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
   use: {
     headless: false,
     baseURL: 'https://demo.haroldwaste.com/',
